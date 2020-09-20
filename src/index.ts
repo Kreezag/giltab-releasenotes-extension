7f1d8355a61375c5ddd21d47e291129e83650a62
Kuchuk Andrey <kreezag@gmail.com>
2020-09-20 23:19:58 +0300

    create test main css file

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,14 @@ require("dotenv").config();
 
 console.log("Hello world");
 
-
 interface Project {
-    readonly id: string;
-    readonly name: string;
+  readonly id: string;
+  readonly name: string;
 }
 
 interface Option {
-    label: string,
-    value: string
+  label: string;
+  value: string;
 }
 
 const site = process.env.RESOURCE || "";
@@ -33,7 +32,8 @@ function api<T>(url: string): Promise<T> {
 
 // Consumer - consumer remains the same
 const projectOptions = api<Project[]>(`${site}/api/v4/projects/`)
-  .then(data => data.map(({ name, id }) => ({
+  .then(data =>
+    data.map(({ name, id }) => ({
       label: name,
       value: id
     }))
@@ -41,6 +41,5 @@ const projectOptions = api<Project[]>(`${site}/api/v4/projects/`)
   .catch(error => {
     /* show error message */
   });
-
 
 console.log(projectOptions);
