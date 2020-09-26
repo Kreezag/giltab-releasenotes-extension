0df60bb6c0fc7c5f962a388e6b5e48548c749c03
Kuchuk Andrey <kreezag@gmail.com>
2020-09-26 19:24:40 +0300

    create request-url helper

---
+++ src/requestUrl.ts
@@ -0,0 +1,47 @@
+const isEmpty = require('lodash/isEmpty')
+
+interface RequestUrlParamsSearch {
+    [key: string]: string;
+}
+
+interface RequestUrlParams {
+    pathname: string;
+    search?: RequestUrlParamsSearch|null;
+    hash?: string;
+    protocol?: 'http'|'https';
+}
+
+const isStartWithSlash = (string: string) => {
+    const trimString = string.trim()
+
+    return trimString.startsWith('/')
+}
+
+const convertSearchParams = (params: RequestUrlParamsSearch|null) => {
+    if (!isEmpty(params)) {
+        return Object.entries(params.search).reduce((result, [key, value]) => result ? `${result}&${key}=${value}` : `?${key}=${value}`, '')
+    }
+    return ''
+}
+
+const createRequestUrl = (
+    origin: string,
+    params: RequestUrlParams = {
+        pathname: '',
+        protocol: 'http',
+        search: null,
+        hash: null,
+    } ) => {
+    const _pathname = isStartWithSlash(params.pathname) ? params.pathname : `/${params.pathname}`;
+    const _protocol = origin.trim().match('/http|https:\/\//') ? '' : params.protocol;
+    const _origin = origin.replace('/\/$/', '');
+    const _search = convertSearchParams(params.search);
+    const _hash = params.hash ? `#${params.hash}` : '';
+
+    const url = new URL (`${_protocol}://${_origin}${_pathname}${_search}${_hash}`)
+
+    return url.toString()
+}
+
+
+export { createRequestUrl }
