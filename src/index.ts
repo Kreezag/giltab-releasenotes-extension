257b6f78a8a116b7dd383bafb5e961a283b8d184
Kuchuk Andrey <kreezag@gmail.com>
2020-10-04 14:04:34 +0300

    update extension icon

--- src/index.ts
+++ src/index.ts
@@ -12,18 +12,14 @@ import {
 
 
 chrome.runtime.onInstalled.addListener(function() {
-    // Replace all rules ...
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        // With a new rule ...
         chrome.declarativeContent.onPageChanged.addRules([
             {
-                // That fires when a page's URL contains a 'g' ...
                 conditions: [
                     new chrome.declarativeContent.PageStateMatcher({
                         pageUrl: { urlContains: JIRA_SITE },
                     })
                 ],
-                // And shows the extension's page action.
                 actions: [ new chrome.declarativeContent.ShowPageAction() ]
             }
         ]);
