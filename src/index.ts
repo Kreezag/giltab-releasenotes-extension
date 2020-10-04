758d4e2938bb6c7b115eaff76543283d0d52e78e
Kuchuk Andrey <kreezag@gmail.com>
2020-10-04 13:30:39 +0300

    add extension base popup.add test extension icons

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,24 @@ import {
 } from "./constant";
 
 
+chrome.runtime.onInstalled.addListener(function() {
+    // Replace all rules ...
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        // With a new rule ...
+        chrome.declarativeContent.onPageChanged.addRules([
+            {
+                // That fires when a page's URL contains a 'g' ...
+                conditions: [
+                    new chrome.declarativeContent.PageStateMatcher({
+                        pageUrl: { urlContains: JIRA_SITE },
+                    })
+                ],
+                // And shows the extension's page action.
+                actions: [ new chrome.declarativeContent.ShowPageAction() ]
+            }
+        ]);
+    });
+});
 
 interface Project {
   readonly id: number;
